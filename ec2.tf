b859af7beb240f7e37f9182fdda8682561f9c9f7
festus <festusmbah1@gmail.com>
2022-10-28 13:40:35 -0700

    adding webser infra

--- ec2.tf
+++ ec2.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "web" {
-  ami           = "var.AMI_ID"
-  instance_type = "var.instance_type"
+  ami           = var.AMI_ID
+  instance_type = var.INSTANCE_TYPE
 
   tags = {
     Name = "airbnb-web-server"
